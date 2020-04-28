2eaeba365c4f32d8825cf084e6cd9985fe01ac60
Yaroslav <yaroslav.lukash@ukr.net>
2020-04-28 12:11:29 +0300

    +++

--- science/assets/js/custom.js
+++ science/assets/js/custom.js
@@ -1,6 +1,6 @@
 /*=============================================================
     Authour URL: www.designbootstrap.com
-    
+
     http://www.designbootstrap.com/
 
     License: MIT
@@ -10,7 +10,7 @@
     100% Free To use For Personal And Commercial Use.
 
     IN EXCHANGE JUST TELL PEOPLE ABOUT THIS WEBSITE
-   
+
 ========================================================  */
 
 $(document).ready(function () {
@@ -103,7 +103,11 @@ return false;
 WRITE YOUR CUSTOM SCRIPTS BELOW
 ======================================*/
 
-
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      /*.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  */});
 
 
 
