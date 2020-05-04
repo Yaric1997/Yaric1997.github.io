001e4c47e1308d060348a1fb5a11af1b7bca2083
Yaroslav <yaroslav.lukash@ukr.net>
2020-05-04 18:58:02 +0300

    add wrapper, wrapper-active(style), tab(js), tab, content tab(html)

--- science/Technic/js/script.js
+++ science/Technic/js/script.js
@@ -1,6 +1,6 @@
 /*=============================================================
     Authour URL: www.designbootstrap.com
-    
+
     http://www.designbootstrap.com/
 
     License: MIT
@@ -10,7 +10,7 @@
     100% Free To use For Personal And Commercial Use.
 
     IN EXCHANGE JUST TELL PEOPLE ABOUT THIS WEBSITE
-   
+
 ========================================================  */
 
 $(document).ready(function () {
@@ -105,8 +105,11 @@ WRITE YOUR CUSTOM SCRIPTS BELOW
 
 
 
-
-
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.wrapper').removeClass('wrapper-active').eq($(this).index()).addClass('wrapper-active');
+  });
 
 
 });
